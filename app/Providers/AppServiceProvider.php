ef3e7b2dde434dcf1d5783ad11e8f0445dca8e5c
vagrant <vagrant@homestead>
2017-12-21 05:56:50 +0000

    mysql 5.6 issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Contract;
 use App\Observers\ContractObserver;
 use Encore\Admin\Config\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+		Schema::defaultStringLength(191);
         Contract::observe(ContractObserver::class);
 		//Config::load();
     }
