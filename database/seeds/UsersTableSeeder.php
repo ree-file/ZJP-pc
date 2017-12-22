20b0a30855d9d70a566e269c93ce2961fd9b0f74
vagrant <vagrant@homestead>
2017-12-22 14:24:03 +0000

    dada

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = new \App\User();
-        $user->email = '1205730728@ZJP.com';
+        $user->email = '1205730728@qq.com';
         $user->password = bcrypt('password');
         $user->money_active = 100000;
 		$user->money_market = 100000;
