7d491449c89b26bbd7ac5eadf811e58fd3029b89
vagrant <vagrant@homestead>
2017-12-27 09:10:06 +0000

    pconfig

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@
 if (! function_exists('rand_name')) {
 	function rand_name() {
 		$prefix = chr(mt_rand(65, 90)).chr(mt_rand(65, 90)).chr(mt_rand(65, 90));
-		$code = mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9);
+		$code = mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9).mt_rand(0, 9);
 		return $prefix.$code;
 	}
 }
